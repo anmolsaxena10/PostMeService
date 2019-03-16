21461f6898682b8e790ca45c5eb263ed7befbc07
Anmol Saxena <anmol.saxena10@gmail.com>
2019-03-16 15:32:00 +0530

    basic model classes added

---
+++ PostMeService/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostMeService.Models
+{
+    class User
+    {
+        [Key]
+        public int userId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual ICollection<Post> posts { get; set; }
+        public virtual ICollection<Comment> comments { get; set; }
+    }
+}
